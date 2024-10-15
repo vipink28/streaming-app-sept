ccfebc7fe2bdeeb6e70ba7bd33b8507591c4c9ef
vipink28 <vipinkalyan28@gmail.com>
2024-10-15 18:57:53 +0530

    api key udpate

--- src/helper/apirequests.js
+++ src/helper/apirequests.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://api.themoviedb.org/3';
-const API_KEY = 'b1afec16ca29a99de834626942f6d05d';
+const API_KEY = '' //add your api key here;
 const apirequests = {
     getNetflixOrginals: `${baseUrl}/discover/tv?api_key=${API_KEY}&with_networks=213`
 }
